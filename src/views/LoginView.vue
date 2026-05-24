b7a7c1606fe4e22ca207e6ff15a86aa9538f695e
Hartinna Pro <hartinnapro@gmail.com>
2026-05-24 14:53:31 +0800

    Wrap login view in form for password-manager compatibility

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -16,14 +16,14 @@
       <Transition name="fade" mode="out-in">
 
         <!-- LOGIN -->
-        <div v-if="view === 'login'" key="login">
+        <form v-if="view === 'login'" key="login" @submit.prevent="doLogin">
 
           <div v-if="alert.msg" :class="['alert', 'alert-' + alert.type]">{{ alert.msg }}</div>
 
           <div class="form-group">
             <label for="login-email">Email or Username</label>
             <input id="login-email" ref="emailInputRef" v-model="L.email" type="text" placeholder="you@email.com or your_username"
-              :class="{ err: E.email }" @keyup.enter="doLogin" />
+              :class="{ err: E.email }" />
             <div v-if="E.email" class="field-error">{{ E.email }}</div>
           </div>
 
@@ -32,7 +32,7 @@
             <div class="input-wrap">
               <input id="login-password" v-model="L.password" :type="showPw ? 'text' : 'password'"
                 class="has-toggle" :class="{ err: E.password }"
-                placeholder="Enter your password" @keyup.enter="doLogin" />
+                placeholder="Enter your password" />
               <button class="pass-toggle" :class="{ 'is-visible': showPw }" @click="showPw = !showPw" type="button" tabindex="-1">
                 <svg v-if="!showPw" width="18" height="18" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path d="M1 12s4-8 11-8 11 8 11 8-4 8-11 8-11-8-11-8z"/><circle class="eye-pupil" cx="12" cy="12" r="3"/></svg>
                 <svg v-else width="18" height="18" fill="none" stroke="currentColor" stroke-width="2" viewBox="0 0 24 24"><path d="M17.94 17.94A10.07 10.07 0 0112 20c-7 0-11-8-11-8a18.45 18.45 0 015.06-5.94M9.9 4.24A9.12 9.12 0 0112 4c7 0 11 8 11 8a18.5 18.5 0 01-2.16 3.19m-6.72-1.07a3 3 0 11-4.24-4.24"/><line x1="1" y1="1" x2="23" y2="23"/></svg>
@@ -57,7 +57,7 @@
               :class="{ glassy: isGlassy }"
               @mousedown="triggerGlass"
               @touchstart.passive="triggerGlass"
-              @click="doLogin"
+              type="submit"
               :disabled="loading"
             >
               <span v-if="loading" class="spinner"></span>
@@ -105,7 +105,7 @@
               </svg>
             </button>
           </div>
-        </div>
+        </form>
 
         <!-- REGISTER -->
         <div v-else-if="view === 'register'" key="register">
